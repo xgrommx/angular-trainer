2e82a91e9faeb781aa9f5cfa9847c5c7bb40c2c5
Igor Ivanovic <igor.ivanovic@censhare.de>
2013-11-21 01:31:55 +0100

    Added home controller

---
+++ src/app/shared/controller/at-home/controller.js
@@ -0,0 +1,18 @@
+/**
+ * Created by igi on 17.11.13.
+ */
+angularTrainer.register.controller(
+    'atHome',
+    ['$scope',
+        function ($scope) {
+
+            /**
+             * Page title
+             * @type {string}
+             */
+            $scope.pageTitle = 'Structuring an AngularJs application';
+
+
+        }
+    ]
+)
